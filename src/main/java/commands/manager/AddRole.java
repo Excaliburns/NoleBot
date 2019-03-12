950d5d62c2e93ad75e5c5441cbbd5369ad979ab7
Tut <tutsandallthat@gmail.com>
2019-03-12 09:34:20 -0400

    Fixing up commands

--- src/main/java/commands/manager/AddRole.java
+++ src/main/java/commands/manager/AddRole.java
@@ -1,5 +1,7 @@
 package commands.manager;
 
+import commands.util.Command;
+import commands.util.CommandEvent;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,8 +12,23 @@ import java.util.List;
 import static util.UserHelper.isVerified;
 import static util.UserHelper.nameMatch;
 
-public class AddRole extends ListenerAdapter {
+public class AddRole extends Command {
+
+    public AddRole()
+    {
+        name = "addrole";
+        description = "Adds a role that is lower than your own permission level to a user - if the Guild has allowed it to be.";
+        helpDescription = "This command allows you to assign roles to others that are lower than your own permission level. These roles must be added to the permissions list by your guild admins. Use !listperm to see a list of these commands.";
+        requiredPermission = 500;
+        usages.add("addrole <@User> <@Role>");
+        usages.add("addrole <@Role> [@User, as many as you want] [@Role, as many as you want]");
+    }
+
     @Override
+    public void onCommandReceived(CommandEvent event) {
+
+    }
+
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
         MessageChannel sentChannel = event.getChannel();
