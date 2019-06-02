50412034e3caf14276c73e25039910c6ba60740f
Tut <tutsandallthat@gmail.com>
2019-06-01 21:10:12 -0400

    forgot this one

---
+++ src/main/java/commands/inhouse/InhouseStruct.java
@@ -0,0 +1,16 @@
+package commands.inhouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InhouseStruct {
+    List<Inhouse> inhouses;
+
+    public List<Inhouse> getInhouses() {
+        return inhouses;
+    }
+
+    public void setInhouses(List<Inhouse> inhouses) {
+        this.inhouses = inhouses;
+    }
+}
